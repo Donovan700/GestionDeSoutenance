d8120c7111e88c939585abbecf79077a5042dd1b
Donovan700 <105297551+Donovan700@users.noreply.github.com>
2023-03-29 17:05:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Images/logoENI.png" />
+    <title>GESTION DE SOUTENANCES</title>
+</head>
+<body>
+    <h1>BIENVENUE</h1>
+    <a href="Creation/CreationEtudiant.php"><button>Enregistrer un Etudiant</button></a>
+    <a href="Creation/CreationOrganisme.php"><button>Enregistrer un Organisme</button></a>
+    <a href="Creation/CreationProfesseur.php"><button>Enregistrer un Professeur</button></a>
+    <a href="Creation/CreationSoutenance.php"><button>Enregistrer un Soutenance</button></a><br/><br/>
+    <a href="Read/ReadEtudiant.php"><button>Voir les étudiants</button></a>
+    <a href="Read/ReadOrganisme.php"><button>Voir les Organismes</button></a>
+    <a href="Read/ReadProfesseur.php"><button>Voir les Professeurs</button></a>
+    <a href="Read/ReadSoutenance.php"><button>Voir les soutenances</button></a>
+</body>
+</html>
